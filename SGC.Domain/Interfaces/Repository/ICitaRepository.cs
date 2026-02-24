bad3ccaf24344553872b8622117de336f4851b59
johanvasquezdev <20251235@itla.edu.do>
2026-02-24 18:55:32 -0400

    agrego de notificacion y auditable

--- SGC.Domain/Interfaces/Repository/ICitaRepository.cs
+++ SGC.Domain/Interfaces/Repository/ICitaRepository.cs
@@ -1,12 +1,12 @@
 using SGC.Domain.Entities.Appointments;
+using SGC.Domain.Interfaces.Repository;
 
-public class ICitaRepository // Interfaz para el repositorio de citas, define los métodos que se implementarán para gestionar las citas en la base de datos
-{
-    public interface ICitaRepository : IBaseRepository<Cita>
+
+    public interface ICitaRepository : IBaseRepository <Cita> // Hereda de la interfaz genérica IBaseRepository para aprovechar las operaciones CRUD estándar, y agrega métodos específicos para la entidad Cita.
     {
         Task<IEnumerable<Cita>> GetByPacienteIdAsync(int pacienteId);
         Task<IEnumerable<Cita>> GetByMedicoIdAsync(int medicoId);
         Task<IEnumerable<Cita>> GetByFechaAsync(DateTime fecha);
         Task<bool> ExisteConflictoAsync(int medicoId, DateTime fechaHora);
     }
-}
+
