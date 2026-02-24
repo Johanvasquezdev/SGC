6afa2ec0eb902b2988321015c1ee495709e2c6dd
Gregori Evangelista Jimenez <gregorievangelista43@gmail.com>
2026-02-24 13:45:57 -0400

    Refactor IAuditoriaRepository to interface with methods
    Changed IAuditoriaRepository from a class to an interface and added methods for retrieving audit entities.

--- SGC.Domain/Interfaces/Repository/IAuditoriaRepository.cs
+++ SGC.Domain/Interfaces/Repository/IAuditoriaRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SGC.Domain.Base;
 
 namespace SGC.Domain.Interfaces.Repository
 {
-    internal class IAuditoriaRepository // para registrar auditorías de acciones importantes en el sistema, como creación, actualización o eliminación de citas, usuarios, etc.
+    public interface IAuditoriaRepository : IBaseRepository<AuditEntity>
     {
+        Task<IEnumerable<AuditEntity>> GetByEntidadAsync(string entidad);
+        Task<IEnumerable<AuditEntity>> GetByUsuarioIdAsync(int usuarioId);
+        Task<IEnumerable<AuditEntity>> GetByFechaAsync(DateTime fecha);
     }
 }
