982d20f9472864e9f31693015cbb12561f28741c
Gregori Evangelista Jimenez <gregorievangelista43@gmail.com>
2026-03-04 20:07:37 -0400

    Updated AuditEntity.cs on 2026-03-05

--- SGC.Domain/Base/AuditEntity.cs
+++ SGC.Domain/Base/AuditEntity.cs
@@ -1,8 +1,8 @@
-// Base namespace
-namespace SGC.Domain.Base
+namespace SGC.Domain.Base;
+
+public abstract class AuditEntity : EntidadBase
 {
-    public class AuditEntity : EntidadBase
-    {
-        public int? UsuarioId { get; set; }
-    }
+    public int? UsuarioId { get; set; }
+    public string Accion { get; set; } = string.Empty;
+    public string IP { get; set; } = string.Empty;
 }
