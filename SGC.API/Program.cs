4b0b3559a1055cdde529078de2228a0772c8a303
johanvasquezdev <20251235@itla.edu.do>
2026-03-12 08:05:50 -0400

    cambios

--- SGC.API/Program.cs
+++ SGC.API/Program.cs
@@ -1,8 +1,12 @@
+using SGC.IOC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Registra dependencias de Application y Persistence usando IOC.
+builder.Services.AddSGCDependencies(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
