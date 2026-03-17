0269f09dd19879cf2f276c6bfb54b76e1ebc074d
johanvasquezdev <20251235@itla.edu.do>
2026-03-17 02:20:42 -0400

    actualizacion de entidades y agrego de servicios externos

---
+++ SGC.Domain/Interfaces/Repository/IPagoRepository.cs
@@ -0,0 +1,13 @@
+﻿using SGC.Domain.Entities.Payments;
+using SGC.Domain.Enums;              // ← para EstadoPago
+
+namespace SGC.Domain.Interfaces.Repository
+{
+    public interface IPagoRepository : IBaseRepository<Pago>
+    {
+        Task<Pago?> GetByCitaAsync(int citaId);
+        Task<Pago?> GetByStripeIdAsync(string stripePaymentIntentId);
+        Task<IEnumerable<Pago>> GetByPacienteAsync(int pacienteId);
+        Task<IEnumerable<Pago>> GetByEstadoAsync(EstadoPago estado);
+    }
+}
