93c490669a6e072c70f5405287e22a0fbf739337
johanvasquezdev <20251235@itla.edu.do>
2026-03-17 02:36:43 -0400

    agregar repository admin

---
+++ SGC.Domain/Interfaces/Repository/IAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using SGC.Domain.Entities.Security;
+using System.Threading.Tasks;
+
+namespace SGC.Domain.Interfaces.Repository
+{
+    // Interfaz para el repositorio de administradores, que hereda de la interfaz base de repositorios y agrega métodos específicos para la entidad Administrador
+    public interface IAdministradorRepository
+        : IBaseRepository<Administrador>
+    {
+        Task<Administrador?> GetByEmailAsync(string email);
+    }
+}
