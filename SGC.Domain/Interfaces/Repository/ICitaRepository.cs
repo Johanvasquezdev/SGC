d33723157c2b8a0fb5506d2e4049991c5babe57b
Gregori <user@LAPTOP-IKV6LTI0>
2026-03-23 07:48:35 -0400

    feat: implementar lógica completa de Citas (Domain, App, Persistence y API)

--- SGC.Domain/Interfaces/Repository/ICitaRepository.cs
+++ SGC.Domain/Interfaces/Repository/ICitaRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace SGC.Domain.Interfaces.Repository;
+using SGC.Domain.Entities.Appointments;
 
+<<<<<<< HEAD
 public interface ICitaRepository // Interfaz para el repositorio de citas, define los métodos que se implementarán para gestionar las citas en la base de datos
+=======
+namespace SGC.Domain.Interfaces.Repository
+>>>>>>> c200be8 (Refactor ICitaRepository to interface and add methods)
 {
-    
+    public interface ICitaRepository : IBaseRepository<Cita>
+    {
+        Task<IEnumerable<Cita>> GetByPacienteIdAsync(int pacienteId);
+        Task<IEnumerable<Cita>> GetByMedicoIdAsync(int medicoId);
+        Task<IEnumerable<Cita>> GetByFechaAsync(DateTime fecha);
+        Task<bool> ExisteConflictoAsync(int medicoId, DateTime fechaHora);
+    }
 }
